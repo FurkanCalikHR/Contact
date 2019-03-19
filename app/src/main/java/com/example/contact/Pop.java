34529d69c9e8e044bccf507ab1576281007f718c
Furkan <0928212@hr.nl>
2019-03-19 11:10:58 +0100

    pop

--- app/src/main/java/com/example/contact/Pop.java
+++ app/src/main/java/com/example/contact/Pop.java
@@ -17,6 +17,7 @@ public class Pop extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
+        getWindow().setLayout((int)(width * .8),(int)(height*.6));
 
     }
 }
