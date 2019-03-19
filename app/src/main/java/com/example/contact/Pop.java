ea8d36e024f751e5d90b4b527b361bda9413575b
Furkan <0928212@hr.nl>
2019-03-19 09:28:55 +0100

    Fixed + New code(Not working still)

---
+++ app/src/main/java/com/example/contact/Pop.java
@@ -0,0 +1,17 @@
+package com.example.contact;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class Pop extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popwindow);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+    }
+}
